aa8507eff10a7364c9c1dc73053ba97357110f00
fractalwrench <fractalwrench@gmail.com>
2021-07-11 19:56:23 +0100

    answer ch6q1: finding the bottle with heavy pills

---
+++ src/main/java/co/uk/fractalwrench/cci/ch6/Chapter6Question1.java
@@ -0,0 +1,19 @@
+package co.uk.fractalwrench.cci.ch6;
+
+public class Chapter6Question1 {
+
+    /*
+     * Question: you have 20 bottles of pills, 19 of which have 1.0 gram pills, but one has pills weighing 1.1 grams.
+     * Given a perfect scale, how would the heavy bottle be detected by using the scale only once?
+     *
+     * As the scale can only be used once it will be necessary to weigh at least 19 of the bottles to distinguish the
+     * one containing heavy pills.
+     *
+     * Each bottle should be labelled 1-n. For each bottle n pills should be placed on the scale. This means that
+     * n heavy pills will be placed on the scale, causing a weight discrepancy of 0.1 * n grams. This discrepancy
+     * will allow the bottle containing the heavy pills to be detected.
+     *
+     * This solution assumes that each bottle contains at least n pills.
+     */
+
+}
