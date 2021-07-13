30064fde8730e9d8551661cae197cb49e97ab9ad
fractalwrench <fractalwrench@gmail.com>
2021-07-13 18:51:28 +0100

    ch13q1

---
+++ src/main/java/co/uk/fractalwrench/cci/ch13/Chapter13Question1.java
@@ -0,0 +1,10 @@
+package co.uk.fractalwrench.cci.ch13;
+
+public class Chapter13Question1 {
+
+    /*
+     * Making a constructor private in Java marks it as inaccessible to anything other than its own inner classes.
+     * This of course can be circumvented with reflection.
+     */
+
+}
