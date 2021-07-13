b4e22ee028f97a78a72cd6c47f33893f3cead846
fractalwrench <fractalwrench@gmail.com>
2021-07-13 18:48:10 +0100

    ch11q1

---
+++ src/main/java/co/uk/fractalwrench/cci/ch11/Chapter11Question1.java
@@ -0,0 +1,10 @@
+package co.uk.fractalwrench.cci.ch11;
+
+public class Chapter11Question1 {
+
+    /*
+     * Mistake 1: the for loop will not terminate as unsigned ints are always greater than or equal to zero.
+     * Mistake 2: The format specifier is wrong and should be %ud
+     */
+
+}
